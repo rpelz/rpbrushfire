e0a67072eb69dd62d07fc627a69ec4ba8a35cfb7
Robert Pelz <rpelz@icloud.com>
2015-05-27 22:52:23 +0200

    Change to angular.js

--- assets/js/videos-page.js
+++ assets/js/videos-page.js
@@ -1,6 +1,67 @@
+/// <reference path="../../typings/angularjs/angular.d.ts"/>
 /// <reference path="../../typings/lodash/lodash.d.ts"/>
 /// <reference path="../../typings/jquery/jquery.d.ts"/>
-$(function whenDomIsReady() {
+
+angular.module('brushfire_videosPage', [])
+.config(function ($sceDelegateProvider) {
+	$sceDelegateProvider.resourceUrlWhitelist([
+		'self',
+		'*://www.youtube.com/**'
+	]);
+});
+
+angular.module('brushfire_videosPage')
+.controller('PageCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+	$scope.videosLoading = true;
+	
+	$timeout(function afterRetrievingVideos() {
+		var _videos = [{
+			title: 'PSY - GANGNAM STYLE (강남스타일) M/V',
+			src: 'https://www.youtube.com/embed/9bZkp7q19f0'
+		}, {
+			title: 'ustin Bieber - Baby ft. Ludacris',
+			src: 'https://www.youtube.com/embed/kffacxfA7G4'
+		}, {
+			title: 'Charlie bit my finger - again !',
+			src: 'https://www.youtube.com/embed/_OBlgSz8sSM'
+		}];
+		
+		$scope.videosLoading = false;
+	
+		$scope.videos = _videos;
+	}, 750);
+	
+	$scope.submitNewVideo = function () {
+		if ($scope.busySubmittingVideo) {
+			return;
+		}
+		
+		var _newVideo = {
+			title: $scope.newVideoTitle,
+			src: $scope.newVideoSrc
+		};
+		
+		var parser = document.createElement('a');
+		parser.href = _newVideo.src;
+		var youtubeID = parser.search.substring(parser.search.indexOf("=")+1, parser.search.length);
+		_newVideo.src = 'https://www.youtube.com/embed/' + youtubeID;
+		
+		$scope.busySubmittingVideo = true;
+		
+		$timeout(function () {
+			$scope.videos.unshift(_newVideo);
+			$scope.busySubmittingVideo = false;
+			$scope.newVideoTitle = '';
+			$scope.newVideoSrc = '';
+		}, 750);
+	};
+}]);
+
+
+
+
+
+/*$(function whenDomIsReady() {
 	$('.the-submit-video-form').submit(function (e) {
 		e.preventDefault();
 		
@@ -57,4 +118,4 @@ $(function whenDomIsReady() {
 		$('.the-list-of-videos ul').replaceWith(videosHtml);
 		
 	}, 750);
-});
+});*/
---
+++ typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
